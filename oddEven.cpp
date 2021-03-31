031683fa6e9a8902c982a05b2c5942646553fa24
munish-garg <munishkumar123264@gmail.com>
2021-03-31 19:29:17 +0530

    oddEven

---
+++ oddEven.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n; cin>>n;
+    if(n&1){
+        cout<<"The number "<<n<<" is odd."<<endl;
+    }
+    else{
+        cout<<"The number "<<n<<" is odd."<<endl;
+    }
+    return 0;
+}
